de05a0226a68ece8e009c47ebec63d73dfb64f55
ng-23 <23grattan@gmail.com>
2025-02-27 14:39:17 -0500

    Added insertion of dummy users to DB

--- src/harsh_engine/app/database.py
+++ src/harsh_engine/app/database.py
@@ -1,33 +1,44 @@
 import sqlite3
 from flask import current_app, g
+from harsh_engine.app.model import data_mappers
+from harsh_engine.app import utils
 
 # code largely taken from the Flask SQLite example
 # see https://flask.palletsprojects.com/en/stable/patterns/sqlite3/
-
 
 def get_db() -> sqlite3.Connection:
     """
     Establishes a database connection and stores it in the current (global) app/request context
     """
+
     if "db" not in g:
         g.db = sqlite3.connect(current_app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES)
         g.db.row_factory = sqlite3.Row
 
     return g.db
 
-
 def init_db(schema: str | None = None):
     """
     Creates a database according to the provided schema and defines how to shut it down when the current app context is done
+
+    Also populates database with some dummy users
     """
+
     db = get_db()
 
     if schema is not None:
         with current_app.open_resource(schema) as f:
             db.executescript(f.read().decode("utf8"))
 
-    current_app.teardown_appcontext(close_db)
+    users = utils.generate_dummy_users(n_users=current_app.config['N_DUMMY_USERS'], seed=current_app.config['SEED'])
 
+    data_mapper = data_mappers.UserMapper()
+    data_mapper.db = db
+
+    for user in users:
+        _ = data_mapper.create(user)
+
+    current_app.teardown_appcontext(close_db)
 
 def close_db(exception = None):
     db = g.pop("db", None)
---
+++ src/harsh_engine/app/utils.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass, field
+from harsh_engine.app.model import entities
+from typing import List
+import random
+from faker import Faker
+
+@dataclass(frozen=True)
+class ModelState:
+    """
+    Represents the state of the Model layer after the processing of a request by the user
+    """
+
+    valid: bool = False
+    message: str = ""
+    data: List[any] = field(default_factory=list) # see https://stackoverflow.com/questions/53632152/why-cant-dataclasses-have-mutable-defaults-in-their-class-attributes-declaratio
+    errors: List[Exception] = field(default_factory=list)  
+
+def generate_dummy_users(n_users=10, seed=42):
+    '''
+    Generates `n_users` dummy/mock users
+    '''
+
+    bad_passwords = ['123ABC!!!','password1','sunshine','blink182','iloveyou2','trustno1']
+
+    users = []
+
+    Faker.seed(seed)
+    fake = Faker()
+
+    temp_rand = random.Random(x=seed)
+
+    for i in range(n_users):
+        user = entities.User(
+            fake.user_name(),
+            password=temp_rand.choice(bad_passwords),
+            password_is_hashed=False,
+        )
+        users.append(user)
+
+    return users
