10bded6989432b05a059f73b012468ac09d1f8d2
ng-23 <23grattan@gmail.com>
2025-02-27 22:57:18 -0500

    Usernames are now unique

--- src/harsh_engine/app/data/schema.sql
+++ src/harsh_engine/app/data/schema.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     id INTEGER NOT NULL UNIQUE PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
+    username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
     join_time INTEGER NOT NULL,
     last_seen_time INTEGER NOT NULL
